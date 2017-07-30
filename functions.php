1e1e8c7a7792e67e3de130f80a7f03b1cf29f76d
Marwan Nabil <marwan.nabil85@yahoo.com>
2017-07-30 14:21:08 +0200

    this is my first commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function func_name()
+{
+	echo 'hello';
+}
+
+function adding($x=1, $y=0)
+{
+	return $x+$y;
+}
+
+echo adding(10, 20);
+//echo $value;
---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(0);
+
+
+
+?>
+
+<h1>My Website</h1>
